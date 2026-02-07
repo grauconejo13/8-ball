25e726f9f47d521e3cc53b23b58457ae5c118ce6
Vanessa Victorino <vanessaf.victorino@yahoo.com>
2026-02-07 13:33:34 -0600

    chore: stabilize tailwind v3 with vite + react + esm

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-blue-600 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-white">Tailwind browser test</h1>
+    </div>
+  );
+}
